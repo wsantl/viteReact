631f14d746a9c63798b1afeb060429b42ba7bef9
wsantl <wsantoslba@hotmail.com>
2024-11-14 14:05:16 -0300

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './style.css'
+
+function Home() {
+
+  return (
+    <>
+      <div>
+       <h1>Projeto React + Vite</h1>
+      </div>
+    </>
+  )
+}
+
+export default Home
